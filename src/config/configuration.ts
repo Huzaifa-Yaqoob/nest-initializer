9a61c99f1a115a5d6ee6fc42b64ded99cc93b264
Muhammad Huzaifa Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2025-06-23 20:46:25 +0500

    format change of configure

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,11 +1,15 @@
 export default () => ({
   port: process.env.PORT,
+
   environment: process.env.NODE_ENV,
   isProduction: process.env.NODE_ENV === 'production',
+
   mongoUri: process.env.MONGO_URI,
+
   jwtSecret: process.env.JWT_SECRET,
   accessJwtExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN || '3h',
   refreshJwtExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN || '1w',
+
   accessCookieExpiresIn:
     Number(process.env.ACCESS_COOKIE_EXPIRES_IN) || 10800000,
   refreshCookieExpiresIn:
